4bba051aa9f22161a5deacf2a3c9487ef56f3abf
penspanic <penspanic@gmail.com>
2016-01-24 21:10:54 +0900

    Mediator pattern main class commit

---
+++ Mediator Pattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DP_STUDY.Mediator;
+
+namespace DP_STUDY
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            RankGameRoom room101 = new RankGameRoom();
+
+            Player player01 = new Player("penspanic");
+            Player player02 = new Player("Geun Hee");
+
+            player01.Join(room101);
+            player02.Join(room101);
+
+            room101.GameStart();
+
+        }
+    }
+}
